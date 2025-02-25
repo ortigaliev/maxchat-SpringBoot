1e41b7878157a6170247159c130496ef2b08b130
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2025-02-25 15:54:43 +0900

    Bad request 400

--- src/main/java/api/maxchat/maxchat/controller/ExceptionHandlerController.java
+++ src/main/java/api/maxchat/maxchat/controller/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package api.maxchat.maxchat.controller;
 
+import api.maxchat.maxchat.excp.AppBadException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlerController {
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handle(AppBadException e) {
-//        return ResponseEntity.badRequest().body(e.getMessage());
-//    }
+    @ExceptionHandler(AppBadException.class)
+    public ResponseEntity<String> handle(AppBadException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handle(RuntimeException e) {

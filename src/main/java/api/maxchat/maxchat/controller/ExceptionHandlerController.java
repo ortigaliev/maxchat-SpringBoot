7985ab1c5e839cc989cf1075ef27f32dfd5cfdd0
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2025-02-25 15:39:25 +0900

    Databasedan tableni o'chirib boshqatan yozdik. alter table profile drop constraint if exists profile_status_check

--- src/main/java/api/maxchat/maxchat/controller/ExceptionHandlerController.java
+++ src/main/java/api/maxchat/maxchat/controller/ExceptionHandlerController.java
@@ -1,6 +1,5 @@
 package api.maxchat.maxchat.controller;
 
-import api.maxchat.maxchat.excp.AppBadException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlerController {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handle(AppBadException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handle(AppBadException e) {
+//        return ResponseEntity.badRequest().body(e.getMessage());
+//    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handle(RuntimeException e) {
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body(e.getMessage());
     }
 }

dc96b288cf70c76c7e4a60067dfc1b7cdfed5b82
OP-max <ortigaliyevm@gmail.com>
2025-06-17 10:40:48 +0900

    feat: Generate JWT part-1

--- src/main/java/api/maxchat/maxchat/utils/JwtUtil.java
+++ src/main/java/api/maxchat/maxchat/utils/JwtUtil.java
@@ -1,23 +1,39 @@
 package api.maxchat.maxchat.utils;
 
+import api.maxchat.maxchat.enums.ProfileRole;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JwtUtil {
     private static final int tokenLiveTime = 1000 * 3600 * 24; // 1-day
     private static final String secretKey = "veryLongSecretmazgillattayevlasharaaxmojonjinnijonsurbetbekkiydirhonuxlatdibekloxovdangasabekochkozjonduxovmashaynikmaydagapchishularnioqiganbolsangizgapyoqaniqsizmazgi";
 
-    public static String encode(Integer id) {
+    public static String encode(Integer id, List<ProfileRole> roleList) {
+        /*ROLE_USER, ROLE_ADMIN*/
+        String strRoles = roleList.stream().map(Enum::name).collect(Collectors.joining(","));
+
+        /*Yuqoridagi ol versioni*/
+        /*Map<Srting> strList = new LinkedList<>();
+        for(ProfileRole role : roleList){
+            strList.add(role.name());
+        }
+        String roleString = String.join(",", strList);*/
+
+        Map<String, String> claims = new HashMap<>();
+        claims.put("role", strRoles);
+
+
         return Jwts
                 .builder()
                 .subject(String.valueOf(id))
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .expiration(new Date(System.currentTimeMillis() + tokenLiveTime))
                 .signWith(getSignInKey())
                 .compact();
     }

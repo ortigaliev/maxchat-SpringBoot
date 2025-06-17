4e4cb063fad6f6b476e8b8ae11ea1daf119ebe3e
OP-max <ortigaliyevm@gmail.com>
2025-06-17 14:19:24 +0900

    feat: PostCreate Test Request

---
+++ src/main/java/api/maxchat/maxchat/controller/PostController.java
@@ -0,0 +1,15 @@
+package api.maxchat.maxchat.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gossip")
+public class PostController {
+
+    @PostMapping("/create")
+    public String create() {
+        return "DONE";
+    }
+}
